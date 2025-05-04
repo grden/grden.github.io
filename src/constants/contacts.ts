974c1bec40029ff5d02409a0eac1876d639041e0
Jeongwon Kim <mickykim4@gmail.com>
2025-05-04 14:18:42 +0900

    fix: fixed problem with deployment

--- src/constants/contacts.ts
+++ src/constants/contacts.ts
@@ -1,4 +1,4 @@
 export const EMAIL = "micky4@khu.ac.kr";
 export const GITHUB = "https://github.com/grden";
 export const INSTAGRAM = "https://www.instagram.com/kimjngwon/";
-export const RESUME = "/resume.pdf";
+export const RESUME = "resume.pdf";
